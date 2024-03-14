c45b9375992a50484e5cb91027bbcba5dd59610e
Christopher <117781333+chrism-la@users.noreply.github.com>
2024-03-14 05:37:05 -0700

    recursive and iterative

--- functions.py
+++ functions.py
@@ -1,4 +1,27 @@
+#1.
 def max_num(num1, num2, num3):
   return max(num1, num2, num3)
 result = max_num(10, 5, 20)
 print("Maximum number:", result)
+
+#2.
+#iterative approach 
+'''
+def mult_list(list):
+  result = 1
+  for num in list:
+    result *= num
+  return result
+'''
+#recursive
+def mult_list(list):
+  if len(list) == 0:
+    return 1
+  else:
+    return list[0] * mult_list(list[1:])
+
+numbers = [2, 2, 2, 3]
+result = mult_list(numbers)
+print(result)
+
+#3.
